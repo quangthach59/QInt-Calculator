7fab71c8f076f6a8de090fa8a985f4f36e5bef59
THẠCH NGUYỄN QUANG <1612602@student.hcmus.edu.vn>
2018-03-21 23:01:37 +0700

    Create first solution and items

---
+++ QInt Calculator/QInt Calculator/QintC.h
@@ -0,0 +1,70 @@
+#pragma once
+
+namespace QIntCalculator {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for QintC
+	/// </summary>
+	public ref class QintC : public System::Windows::Forms::Form
+	{
+	public:
+		QintC(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~QintC()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// QintC
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 21);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(426, 422);
+			this->Font = (gcnew System::Drawing::Font(L"Segoe UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
+			this->MaximizeBox = false;
+			this->Name = L"QintC";
+			this->Text = L"QInt Calculator";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
